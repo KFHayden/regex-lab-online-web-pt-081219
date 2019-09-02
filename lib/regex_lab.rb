3dfd518dee3e8596939d95c02b3c75dfeed0968a
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-02 22:13:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,9 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   first_letter = text.scan(/\w+/)[0]
-  if (text.scan)
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+    true
+  end
 end
 
 def valid_phone_number?(phone)
